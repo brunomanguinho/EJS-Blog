011c016b775369d4cee3366be0b903ded218d86d
brunomanguinho <brunomanguinho@gmail.com>
2022-02-17 20:53:13 -0300

    Complete all the app brewery challenges

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,14 +11,51 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home", {ejsHomeStartingContent: homeStartingContent, ejsPosts: posts});
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {ejsAboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res){
+  res.render("contact", {ejsContactContent: contactContent});
+});
 
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.title,
+    content: req.body.text
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/posts/:post_id", function(req, res){
+  // if (posts.find(p => _.lowerCase(p.title) === _.lowerCase(req.params.post_id))){
+  //   res.render("post", {ejsPost: p});
+  // } else console.log("no match");
+
+  posts.forEach(function(post){
+    if (_.lowerCase(post.title) === _.lowerCase(req.params.post_id)){
+      res.render("post", {ejsPost: post});
+    } else console.log("no match");
+  })
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
